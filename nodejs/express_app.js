7679d002bcfd43e3e71ecd0fe7f8b464eded5fc3
junaidkpm1998 <mohdjunaidkp@gmail.com>
2019-04-08 16:33:00 +0530

    express class added to git

--- nodejs/express_app.js
+++ nodejs/express_app.js
@@ -1,11 +1,12 @@
 const express =require('express');//loading express
 var app =express();//function aakki  variable kk vechu
 app.get('/',(req,res)=>{
-    res.send("<marquee><b>u r welcome</b></marquee>")
+    res.send('[{"name":"junaid","age":21},{"name":"arjun","age":24}]')
+   // res.send("<marquee><b>u r welcome</b></marquee>")
 
 });
 app.get('/home',(req,res)=>{
-    res.send("<marquee><b>u r welcome to home</b></marquee>")
+    res.send("<h1 style=color:green><marquee><b>  u r welcome to home</b></marquee></h1>")
 });
 app.get('/contact',(req,res)=>{
     res.send("this is contact page")
