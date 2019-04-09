28120c65fe120a1c316e4b6ebb5743cf8e65bade
junaidkpm1998 <mohdjunaidkp@gmail.com>
2019-04-09 11:21:10 +0530

    express js added

--- nodejs/express_app.js
+++ nodejs/express_app.js
@@ -1,16 +1,28 @@
 const express =require('express');//loading express
-var app =express();//function aakki  variable kk vechu
+const exphbs=require('express-handlebars');
+
+var app =express();
+
+//function aakki  variable kk vechu
+
+app.engine('handlebars',exphbs());
+app.set('view engine','handlebars');
 app.get('/',(req,res)=>{
-    res.send('[{"name":"junaid","age":21},{"name":"arjun","age":24}]')
+     res.render('index')
+//app.get('/',(req,res)=>{
+   // res.send('[{"name":"junaid","age":21},{"name":"arjun","age":24}]')
    // res.send("<marquee><b>u r welcome</b></marquee>")
 
 });
 app.get('/home',(req,res)=>{
-    res.send("<h1 style=color:green><marquee><b>  u r welcome to home</b></marquee></h1>")
+     res.render('home');
 });
+//app.get('/home',(req,res)=>{
+  //  res.send("<h1 style=color:green><marquee><b>  u r welcome to home</b></marquee></h1>")
+//});
 app.get('/contact',(req,res)=>{
     res.send("this is contact page")
 
 });
 
-app.listen(3000)//local host:(/)3000 chromeil adikkuka
+app.listen(3002)//local host:(/)3000 chromeil adikkuka
